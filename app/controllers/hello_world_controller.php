86398048feed5a96a658b559bfa63be49966ae4b
PauliNiva <Pauli@Vulcan.local>
2015-12-19 19:16:52 +0200

    Added initial plans

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -4,11 +4,31 @@
 
     public static function index(){
       // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
-   	  View::make('home.html');
+   	  View::make('/plans/frontpage.html');
     }
 
     public static function sandbox(){
       // Testaa koodiasi täällä
-      echo 'Hello World!';
+      View::make('helloworld.html');
+    }
+
+    public static function listDrinks(){
+      // Testaa koodiasi täällä
+      View::make('/plans/drinklist.html');
+    }
+
+    public static function specificDrink(){
+      // Testaa koodiasi täällä
+      View::make('/plans/specificdrink.html');
+    }
+
+    public static function editPage(){
+      // Testaa koodiasi täällä
+      View::make('/plans/editpage.html');
+    }
+
+    public static function favorites(){
+      // Testaa koodiasi täällä
+      View::make('/plans/favorites.html');
     }
   }
--- config/routes.php
+++ config/routes.php
@@ -7,3 +7,21 @@
   $routes->get('/hiekkalaatikko', function() {
     HelloWorldController::sandbox();
   });
+
+  $routes->get('/drinklist', function() {
+    HelloWorldController::listDrinks();
+  });
+
+  $routes->get('/specificdrink', function() {
+    HelloWorldController::specificDrink();
+  });
+
+  $routes->get('/editpage', function() {
+    HelloWorldController::editPage();
+  });
+
+  $routes->get('/favorites', function() {
+    HelloWorldController::favorites();
+  });
+
+
