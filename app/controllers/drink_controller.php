4b7d9b49fe8cfc4413e51e8ee785be39a4439f63
PauliNiva <Pauli@Vulcan.local>
2016-01-10 21:06:56 +0200

    Refactored method to smaller sizes

--- app/controllers/drink_controller.php
+++ app/controllers/drink_controller.php
@@ -48,45 +48,87 @@ class DrinkController extends BaseController {
 		$modifiedDrink = Drink::findOne($id);
 		$name = $_POST['drink_name'];
 		$errors = Drink::validateEditedDrink_name($name);
+		DrinkController::validateNameErrors($errors, $id);
 		$ingredients = $_POST['ingredients'];
-		foreach ($ingredients as $ingredient) {
-			$ingredient_errors = Ingredient::validateIngredient_name($ingredient);      
-		}
-		if (count($ingredient_errors) > 0) {
-			$drink = Drink::findOne($id);
-			$drink_types = DrinkType::listDrinkTypes();
-			$drink_ingredients = DrinkIngredient::listDrinkIngredients($id);
-			View::make('drink/edit.html', array('attributes' => $drink, 'drink_types' => $drink_types,
-				'drink_ingredients' => $drink_ingredients, 'message' => $ingredient_errors[0]));
-  		}
+		DrinkController::validateIngredientsEdit($name, $ingredients, $id);
 		$amounts = $_POST['amounts'];
-		foreach ($amounts as $amount) {
-    		$amount_errors = DrinkIngredient::validateAmount($amount);
-    	}
-    	if (count($amount_errors) > 0) {
-    		$drink = Drink::findOne($id);
-			$drink_types = DrinkType::listDrinkTypes();
-			$drink_ingredients = DrinkIngredient::listDrinkIngredients($id);
-			View::make('drink/edit.html', array('attributes' => $drink, 'drink_types' => $drink_types,
-				'drink_ingredients' => $drink_ingredients, 'message' => $amount_errors[0]));
-  		}
+		DrinkController::validateAmountEdit($name, $ingredients, $id, $amounts);	
     	$units = $_POST['units'];
     	$ingredient_ids = $_POST['ingredient_id'];
-
-    	if (count($errors) > 0) {
-    		$drink = Drink::findOne($id);
-			$drink_types = DrinkType::listDrinkTypes();
-			$drink_ingredients = DrinkIngredient::listDrinkIngredients($id);
-			View::make('drink/edit.html', array('attributes' => $drink, 'drink_types' => $drink_types,
-				'drink_ingredients' => $drink_ingredients, 'message' => $errors[0]));
-  		}
-
+    	DrinkController::editIngredients($ingredients, $id, $ingredient_ids, $amounts, $units);
     	$modifiedDrink->setDrink_name($name);
     	$modifiedDrink->setDrink_type($_POST['drink_type']);
     	$modifiedDrink->setInstructions($_POST['instructions']);
     	$modifiedDrink->update();
+    	Redirect::to('/drink/' . $modifiedDrink->getDrink_id(), array('message' => 'Drink has been modified.'));
+  	}
 
-    	$i = 0;
+	public static function store() {
+		$newDrink = new Drink();
+		$mockDrink = new Drink();
+		$drink_types = DrinkType::listDrinkTypes();
+		$name = $_POST['drink_name'];
+		$ingredients = $_POST['ingredients'];
+		$amounts = $_POST['amounts'];
+		$units = $_POST['units'];
+		$mockDrink->setDrink_name($name);
+    	$mockDrink->setDrink_type($_POST['drink_type']);
+    	$mockDrink->setInstructions($_POST['instructions']);
+    	DrinkController::validateNew($name, $mockDrink, $drink_types, $ingredients, $amounts);	
+    	$newDrink->setDrink_name($name);
+    	$newDrink->setDrink_type($_POST['drink_type']);
+    	$newDrink->setInstructions($_POST['instructions']);
+    	$newDrink->setAdder_id($_SESSION['user']);
+    	$newDrink->save();
+    	DrinkController::addIngredients($ingredients, $newDrink, $amounts, $units);
+    	Redirect::to('/drink/' . $newDrink->getDrink_id(), array('message' => 'Drink has been archived.'));
+  	}
+
+  	public static function validateNew($name, $mockDrink, $drink_types, $ingredients, $amounts) {
+  		$errors = Drink::validateDrink_name($name);
+		if (count($errors) > 0) {	
+  			View::make('drink/new.html', array('drink_types' => $drink_types,
+  				'mockDrink' => $mockDrink, 'message' => $errors[0]));
+  		}
+  		foreach ($ingredients as $ingredient) {
+			$ingredient_errors = Ingredient::validateIngredient_name($ingredient);      
+		}
+		if (count($ingredient_errors) > 0) {
+  			View::make('drink/new.html', array('drink_types' => $drink_types,
+  				'mockDrink' => $mockDrink, 'message' => $ingredient_errors[0]));
+  		}
+		foreach ($amounts as $amount) {
+    		$amount_errors = DrinkIngredient::validateAmount($amount);
+    	}
+    	if (count($amount_errors) > 0) {
+  			View::make('drink/new.html', array('drink_types' => $drink_types,
+  				'mockDrink' => $mockDrink, 'message' => $amount_errors[0]));
+  		}
+  	}
+
+  	public static function addIngredients($ingredients, $newDrink, $amounts, $units) {
+  		$i = 0;
+    	foreach ($ingredients as $ingredient) {
+    		$ingredient = strtolower($ingredient);
+            if (Ingredient::alreadyInArchive($ingredient) > 0) {
+                $ingredient_id = Ingredient::alreadyInArchive($ingredient);
+            } else {
+                $newIngredient = new Ingredient();
+                $newIngredient->setIngredient_name($ingredient);           
+                $ingredient_id = $newIngredient->save();
+            }
+    		$newDrinkIngredient = new DrinkIngredient();
+        	$newDrinkIngredient->setDrink_id($newDrink->getDrink_id());
+        	$newDrinkIngredient->setIngredient_id($ingredient_id);
+        	$newDrinkIngredient->setAmount($amounts[$i]);
+        	$newDrinkIngredient->setUnit($units[$i]);
+        	$newDrinkIngredient->save();
+        	$i++;
+    	}
+  	}
+
+  	public static function editIngredients($ingredients, $id, $ingredient_ids, $amounts, $units) {
+  		$i = 0;
     	foreach ($ingredients as $ingredient) {
     		$ingredient = strtolower($ingredient);
             if (Ingredient::alreadyInArchive($ingredient) > 0) {
@@ -102,82 +144,52 @@ class DrinkController extends BaseController {
             	$newIngredient = new Ingredient();
                 $newIngredient->setIngredient_name($ingredient);
                 $ingredient_id = $newIngredient->save();
+                $old_ingredient_id = $ingredient_ids[$i];
             	$newDrinkIngredient = new DrinkIngredient();
     			$newDrinkIngredient->setIngredient_id($ingredient_id);
         		$newDrinkIngredient->setDrink_id($id);
         		$newDrinkIngredient->setAmount($amounts[$i]);
         		$newDrinkIngredient->setUnit($units[$i]);
-        		$newDrinkIngredient->update($ingredient_id);
+        		$newDrinkIngredient->update($old_ingredient_id);
             }
         	$i++;
     	}
-
-    	Redirect::to('/drink/' . $modifiedDrink->getDrink_id(), array('message' => 'Drink has been modified.'));
   	}
 
-	public static function store() {
-		$newDrink = new Drink();
-		$mockDrink = new Drink();
-		$drink_types = DrinkType::listDrinkTypes();
-		$name = $_POST['drink_name'];
-		$ingredients = $_POST['ingredients'];
-		$amounts = $_POST['amounts'];
-		$units = $_POST['units'];
-		$mockDrink->setDrink_name($name);
-    	$mockDrink->setDrink_type($_POST['drink_type']);
-    	$mockDrink->setInstructions($_POST['instructions']);
-
-		$errors = Drink::validateDrink_name($name);
-		if (count($errors) > 0) {	
-  			View::make('drink/new.html', array('drink_types' => $drink_types,
-  				'mockDrink' => $mockDrink, 'message' => $errors[0]));
-  		}
-
-		foreach ($ingredients as $ingredient) {
-			$ingredient_errors = Ingredient::validateIngredient_name($ingredient);      
+  	public static function validateIngredientsEdit($name, $ingredients, $id) {
+  		foreach ($ingredients as $ingredient) {
+			$ingredient_errors = Ingredient::validateIngredient_name($ingredient);
+			if (count($ingredient_errors) > 0) {
+				$drink = Drink::findOne($id);
+				$drink_types = DrinkType::listDrinkTypes();
+				$drink_ingredients = DrinkIngredient::listDrinkIngredients($id);
+				View::make('drink/edit.html', array('attributes' => $drink, 'drink_types' => $drink_types,
+					'drink_ingredients' => $drink_ingredients, 'message' => $ingredient_errors[0]));
+  			}   
 		}
+  	}
 
-		if (count($ingredient_errors) > 0) {
-  			View::make('drink/new.html', array('drink_types' => $drink_types,
-  				'mockDrink' => $mockDrink, 'message' => $ingredient_errors[0]));
-  		}
-		
-		foreach ($amounts as $amount) {
+  	public static function validateAmountEdit($name, $ingredients, $id, $amounts) {
+  		foreach ($amounts as $amount) {
     		$amount_errors = DrinkIngredient::validateAmount($amount);
-    	}
-
-    	if (count($amount_errors) > 0) {
-  			View::make('drink/new.html', array('drink_types' => $drink_types,
-  				'mockDrink' => $mockDrink, 'message' => $amount_errors[0]));
+    		if (count($amount_errors) > 0) {
+    			$drink = Drink::findOne($id);
+				$drink_types = DrinkType::listDrinkTypes();
+				$drink_ingredients = DrinkIngredient::listDrinkIngredients($id);
+				View::make('drink/edit.html', array('attributes' => $drink, 'drink_types' => $drink_types,
+					'drink_ingredients' => $drink_ingredients, 'message' => $amount_errors[0]));
+  			}
   		}
-    	
-    	$newDrink->setDrink_name($name);
-    	$newDrink->setDrink_type($_POST['drink_type']);
-    	$newDrink->setInstructions($_POST['instructions']);
-    	$newDrink->setAdder_id($_SESSION['user']);
-    	$newDrink->save();
+  	}
 
-    	$i = 0;
-    	foreach ($ingredients as $ingredient) {
-    		$ingredient = strtolower($ingredient);
-            if (Ingredient::alreadyInArchive($ingredient) > 0) {
-                $ingredient_id = Ingredient::alreadyInArchive($ingredient);
-            } else {
-                $newIngredient = new Ingredient();
-                $newIngredient->setIngredient_name($ingredient);           
-                $ingredient_id = $newIngredient->save();
-            }
-
-    		$newDrinkIngredient = new DrinkIngredient();
-        	$newDrinkIngredient->setDrink_id($newDrink->getDrink_id());
-        	$newDrinkIngredient->setIngredient_id($ingredient_id);
-        	$newDrinkIngredient->setAmount($amounts[$i]);
-        	$newDrinkIngredient->setUnit($units[$i]);
-        	$newDrinkIngredient->save();
-        	$i++;
-    	}
-
-    	Redirect::to('/drink/' . $newDrink->getDrink_id(), array('message' => 'Drink has been archived.'));
+  	public static function validateNameErrors($errors, $id) {
+  		if (count($errors) > 0) {
+    		$drink = Drink::findOne($id);
+			$drink_types = DrinkType::listDrinkTypes();
+			$drink_ingredients = DrinkIngredient::listDrinkIngredients($id);
+			View::make('drink/edit.html', array('attributes' => $drink, 'drink_types' => $drink_types,
+				'drink_ingredients' => $drink_ingredients, 'message' => $errors[0]));
+  		}
   	}
 
   	public static function showSearchPage() {
